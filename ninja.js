a99b42d986aa6ca55b330a94fd08a88629cff4c9
masonhuh <mth517@gmail.com>
2021-09-03 03:09:07 -0500

    commit 2

--- ninja.js
+++ ninja.js
@@ -1,5 +1,5 @@
 class Ninja {
-    constructor(name, health = 70, speed = 2, strength = 6){
+    constructor(name, health = 70, speed = 3, strength = 3){
         this.name = name;
         this.health = health;
         this.speed = speed;
@@ -19,9 +19,23 @@ class Ninja {
     }
 }
 
-const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();
-ninja1.showStats();
-ninja1.takeNap();
+//const ninja1 = new Ninja("Hyabusa");
+//ninja1.sayName();
+//ninja1.showStats();
+//ninja1.takeNap();
 
+class Sensei extends Ninja{
+    constructor(name, wisdom = 10) {
+        super(name, 200, 10, 10)
+        this.wisdom = wisdom
+    }
 
+    speakWisdom(){
+        this.takeNap()
+        console.log("Learning how to controll one's breathe is learning how to obtain controll of one's self.")
+    }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
